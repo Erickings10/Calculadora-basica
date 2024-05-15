f86e805230f5c4acfc876e5b62a6a3f6ea8b0681
JhojanDavila <144264873+JhojanDavila@users.noreply.github.com>
2024-05-15 00:17:18 -0500

    Agregamos la opcion Raiz Cuadrada

--- Calculadora.py
+++ Calculadora.py
@@ -16,6 +16,9 @@ def division(a, b):
 def potencia(a,b):
     return a ** b
 
+def raizcuadrada(a):
+    return a ** 0.5
+
 def calculadora():
     print("Calculadora básica")
     print("1. Suma")
@@ -23,10 +26,22 @@ def calculadora():
     print("3. Multiplicación")
     print("4. División")
     print("5. Potencia")
+    print("6. Raiz Cuadrada")
     opcion = input("Seleccione una opción: ")
 
-    num1 = int(input("Ingrese el primer número: "))
-    num2 = int(input("Ingrese el segundo número: "))
+    if opcion in ['1','2','3','4''5']:
+        num1 = int (input("Ingrese el primer número"))
+        num2 = int (input("Ingrese el segundo número")) 
+    elif opcion == '6':
+        num1 = int (input("Ingrese el número"))
+        while num1 < 0:
+            print("Error: La raíz cuadrada de un número debe ser positiva. Ingrese nuevamente")
+            num1 = int (input("Ingrese un número nuevamente"))
+        num2 = None
+    else:
+        print("Opcion no valida")
+        return
+
 
     if opcion == '1':
         print("Resultado:", suma(num1, num2))
@@ -38,7 +53,10 @@ def calculadora():
         print("Resultado:", division(num1, num2))
     elif opcion == '5':
         print("Resultado:", potencia(num1,num2))
+    elif opcion =='6':
+        print("Resultado:", raizcuadrada(num1))
     else:
         print("Opción no válida")
+        
 
 calculadora()
