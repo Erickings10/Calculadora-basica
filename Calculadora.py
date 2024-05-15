58dee55a28a782065cb9cd48dbf422c4441e2650
Andres Abanto <andresabanto04@gmail.com>
2024-05-15 00:39:53 -0500

    Agregue la funcion raiz cubica

--- Calculadora.py
+++ Calculadora.py
@@ -19,6 +19,10 @@ def potencia(a,b):
 def raizcuadrada(a):
     return a ** 0.5
 
+def raizcubica(a):
+    return a ** (1/3)
+
+
 def calculadora():
     print("Calculadora básica")
     print("1. Suma")
@@ -27,17 +31,26 @@ def calculadora():
     print("4. División")
     print("5. Potencia")
     print("6. Raiz Cuadrada")
+    print("7. Raiz Cubica")
+    
     opcion = input("Seleccione una opción: ")
 
     if opcion in ['1','2','3','4''5']:
-        num1 = int (input("Ingrese el primer número"))
-        num2 = int (input("Ingrese el segundo número")) 
+        num1 = int (input("Ingrese el primer número: "))
+        num2 = int (input("Ingrese el segundo número: ")) 
     elif opcion == '6':
-        num1 = int (input("Ingrese el número"))
+        num1 = int (input("Ingrese el número: "))
         while num1 < 0:
             print("Error: La raíz cuadrada de un número debe ser positiva. Ingrese nuevamente")
             num1 = int (input("Ingrese un número nuevamente"))
         num2 = None
+    elif opcion == "7":
+        num1 = int (input("Ingrese el número: "))
+        while num1 < 0:
+            print("Error: La raíz cubica de un número debe ser positiva. Ingrese nuevamente")
+            num1 = int (input("Ingrese un número nuevamente"))
+        num2 = None
+
     else:
         print("Opcion no valida")
         return
@@ -55,6 +68,8 @@ def calculadora():
         print("Resultado:", potencia(num1,num2))
     elif opcion =='6':
         print("Resultado:", raizcuadrada(num1))
+    elif opcion =='7':
+        print("Resultado:", raizcubica(num1))
     else:
         print("Opción no válida")
         
